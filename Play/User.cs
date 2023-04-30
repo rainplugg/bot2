bf4a8c8964a6ae1777e3415bf55ac5b61c5e9bef
rainplugg <130517913+rainplugg@users.noreply.github.com>
2023-04-30 13:40:50 +0500

    Add files via upload

---
+++ Play/User.cs
@@ -0,0 +1,17 @@
+﻿namespace Play
+{
+   public class User
+   {
+      public string id { get; set; }
+      public string nickname { get; set; }
+      public int balance { get; set; }
+      public string message { get; set; }
+      public User(string id, string nickname, int balance, string message)
+      {
+         this.id = id;
+         this.nickname = nickname;
+         this.balance = balance;
+         this.message = message;
+      }
+   }
+}
